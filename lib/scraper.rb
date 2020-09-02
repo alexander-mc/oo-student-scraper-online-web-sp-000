ff84353295c73d5c320b837b0ba300bfaf3a09a0
Alexander Clamor <alexander.clamor@gmail.com>
2020-09-02 03:04:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,40 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    profiles = []
+    html = open(index_url)
+    info = Nokogiri::HTML(html)
+
+    info.css("div.student-card").each do |student|
+      profile = {}
+      profile[:name] = student.css("h4.student-name").text
+      profile[:location] = student.css("p.student-location").text
+      profile[:profile_url] = student.css("a").attribute("href").value
+      profiles << profile
+    end
     
+    profiles
   end
 
   def self.scrape_profile_page(profile_url)
+    profile = {}
+    html = open(profile_url)
+    info = Nokogiri::HTML(html)
     
+    info.css("div.social-icon-container a").each do |icon|
+      if icon.attribute("href").value.include?("twitter")
+        profile[:twitter] = icon.attribute("href").value
+      if icon.attribute("href").value.include?("linkedin")
+        profile[:linkedin] = icon.attribute("href").value
+      elsif
+    
+    end
   end
 
 end
 
+Scraper.scrape_profile_page("https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html")
