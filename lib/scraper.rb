dbcf5632226703f6b8d8892e0091c40414a71d10
Alexander Clamor <alexander.clamor@gmail.com>
2020-09-02 03:17:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,12 +34,12 @@ class Scraper
         profile[:blog] = icon.attribute("href").value
       else icon.attribute("href").value.include?("blog")
       end
+    end
       
     profile[:profile_quote] = info.css("div.profile-quote").text
     profile[:bio] = info.css("div.details-container div.description-holder p").text
     
     profile
-    end
   end
 end
 
